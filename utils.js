3214e4ce43d4cb58a371504495a7fcec1a1d3cb9
Priznaer <github.com/Priznaer>
2024-11-14 12:32:13 +0000

    Changed the directory strucure

---
+++ utils.js
@@ -0,0 +1,4 @@
+// Triggering file selction dialog with generic button press
+const file_input = document.getElementById("file-input");
+const generic_btn = document.getElementById("file-select-btn");
+generic_btn.onclick(file_input.click());
